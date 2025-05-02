d5a2b359bac97b13d8608432c66208f16ba912f4
mandelbrojt <luis.moreno@exatec.tec.mx>
2025-05-01 21:39:54 -0600

    Reorder field initialization

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // const prompt = require("prompt-sync")({ sigint: true });
 const Field = require("./Field");
 
-let direction;
-
 const fieldGrid = Field.createFieldGrid(7, 5, 30);
 const field = new Field(fieldGrid);
+
 field.print();
 
-//console.log(field.fieldGrid);
+console.log(field.fieldGrid);
 
-playerCoordinates = field.findElement("*");
-console.log(playerCoordinates);
+playerCoordinates = field.getElementCoordinates("*");
 
+let direction;
 // direction = prompt("\nWhich way? \n⬆️ = w \n➡️ = d \n⬇️ = s \n⬅️ = a \nType your move: ");
