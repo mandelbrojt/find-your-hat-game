30266d2c02030338524abcc413a731bd570f7863
mandelbrojt <luis.moreno@exatec.tec.mx>
2025-05-02 00:57:52 -0600

    Implement first game loop

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-// const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")({ sigint: true });
 const Field = require("./Field");
 
 const fieldGrid = Field.createFieldGrid(7, 5, 30);
 const field = new Field(fieldGrid);
 
-field.print();
+let playerPosition = field.getElementCoordinates("*");
 
-console.log(field.fieldGrid);
+let currentPlayerRow = playerPosition.rowIndex;
+let currentPlayerCol = playerPosition.colIndex;
+let isAlive = true;
+let hasWon = false;
 
-playerCoordinates = field.getElementCoordinates("*");
+while (isAlive && !hasWon) {
+    field.print();
+    let direction = prompt("Which way? \n⬆️ = w \n➡️ = d \n⬇️ = s \n⬅️ = a \nType your move: ");
+    
+    // Update the player's position
+    ({currentPlayerRow, currentPlayerCol, isAlive, hasWon} = field.movePlayer(direction, { rowIndex: currentPlayerRow, colIndex: currentPlayerCol }));
+    
+    // Print the updated grid
+    field.print();
+}
 
-let direction;
-// direction = prompt("\nWhich way? \n⬆️ = w \n➡️ = d \n⬇️ = s \n⬅️ = a \nType your move: ");
+// if (!isAlive) {
+//     console.log("Game Over! You fell into a hole or went out of bounds.");
+// } else if (hasWon) {
+//     console.log("Congratulations! You found the hat.");
+// }
